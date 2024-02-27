16f6354d1b019ba00de9cc354eae868af61a6377
Muhammad Talha Noshahi <talhanoshahi@proton.me>
2024-02-27 18:34:46 +0500

    Changet the blocks in config.h to my liking

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -11,22 +11,20 @@
 #define CLICKABLE_BLOCKS 1
 
 // Control whether a leading delimiter should be prepended to the status.
-#define LEADING_DELIMITER 0
+#define LEADING_DELIMITER 1
 
 // Control whether a trailing delimiter should be appended to the status.
 #define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(cmd, interval, signal).
 #define BLOCKS(X)         \
-    X("sb-mail", 600, 1)  \
-    X("sb-music", 0, 2)   \
-    X("sb-disk", 1800, 3) \
-    X("sb-memory", 10, 4) \
-    X("sb-loadavg", 5, 5) \
-    X("sb-mic", 0, 6)     \
-    X("sb-record", 0, 7)  \
-    X("sb-volume", 0, 8)  \
-    X("sb-battery", 5, 9) \
-    X("sb-date", 1, 10)
-
+    X("~/.local/bin/statusbar/sb_language",	0,  5) \
+	X("~/.local/bin/statusbar/sb_used_memory",	1,  14) \
+	X("~/.local/bin/statusbar/sb_nettraf",	1,  16) \
+	X("~/.local/bin/statusbar/sb_brightness",	0,  2) \
+	X("~/.local/bin/statusbar/sb_volume",	0,  10) \
+	X("~/.local/bin/statusbar/sb_internet",	5,  4) \
+	X("~/.local/bin/statusbar/sb_battery/sb_battery",	5,  3) \
+	X("~/.local/bin/statusbar/sb_power",	0, 12) \
+	X("~/.local/bin/statusbar/sb_clock/sb_clock",	60, 1)
 #endif  // CONFIG_H
